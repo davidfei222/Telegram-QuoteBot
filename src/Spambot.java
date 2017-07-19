fbfa5d8e77bab992b2bbe649e1aa35bd150d9f2e
davidfei222 <dfei@wisc.edu>
2017-07-18 23:23:32 -0500

    Added status message request

--- src/Spambot.java
+++ src/Spambot.java
@@ -2,11 +2,14 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.time.LocalDateTime;
 import java.util.Scanner;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 public class Spambot extends TelegramLongPollingBot {
 	
+	// These variables are for reading config.txt
 	private File config;
 	private Scanner reader;
 	private String user;
@@ -28,8 +31,27 @@ public class Spambot extends TelegramLongPollingBot {
 	@Override
 	public void onUpdateReceived(Update update) 
 	{
-		// TODO Auto-generated method stub
-		System.out.println("Update received at" + LocalDateTime.now());
+		System.out.println("Update received at " + LocalDateTime.now());
+		
+		String message = "";
+		
+		if(update.hasMessage())
+		{
+			message = update.getMessage().getText().toLowerCase();
+		}
+		
+		if(message.equals("!status"))
+		{
+    		SendMessage status = new SendMessage() // Create a SendMessage object with mandatory fields
+                    .setChatId(update.getMessage().getChatId())
+                    .setText("Ready to spam the everliving shit out of someone you hate...");
+            try {
+                sendMessage(status); //Call method to send the message
+                System.out.println("Message sent out at " + LocalDateTime.now());
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+    	}
 	}
 	
 	@Override
