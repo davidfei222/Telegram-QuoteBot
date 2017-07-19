e969712ba0e03daf57f32f01e61035d71f00a639
davidfei222 <dfei@wisc.edu>
2017-07-18 23:10:20 -0500

    Added configuration, sanity check for bot working

--- src/Spambot.java
+++ src/Spambot.java
@@ -1,27 +1,47 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.time.LocalDateTime;
+import java.util.Scanner;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 
 public class Spambot extends TelegramLongPollingBot {
-
+	
+	private File config;
+	private Scanner reader;
+	private String user;
+	private String token;
+	
+	public Spambot()
+	{
+		super();
+		config = new File("config.txt");
+		try {
+			reader = new Scanner(config);
+			user = reader.next();
+			token = reader.next();
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	@Override
-	public void onUpdateReceived(Update arg0) 
+	public void onUpdateReceived(Update update) 
 	{
 		// TODO Auto-generated method stub
-		
+		System.out.println("Update received at" + LocalDateTime.now());
 	}
 	
 	@Override
 	public String getBotUsername() 
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return user;
 	}
 
 	@Override
 	public String getBotToken() 
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return token;
 	}
 
 }
