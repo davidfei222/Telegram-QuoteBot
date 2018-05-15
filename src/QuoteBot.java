0e9b8c6849616488e4e807e33a4a249ed486977b
davidfei222 <dfei@wisc.edu>
2018-05-15 17:12:58 -0500

    Starting over
    We will use a local MySQL database instead of all this stupid file I/O
    shit.

--- src/QuoteBot.java
+++ src/QuoteBot.java
@@ -1,9 +1,5 @@
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Writer;
 import java.time.LocalDateTime;
 import java.util.Scanner;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
@@ -17,7 +13,7 @@ import org.telegram.telegrambots.exceptions.TelegramApiException;
 public class QuoteBot extends TelegramLongPollingBot {
 
 	// These variables are for reading config.txt
-	private String workingDir;
+	private String confDir = "/home/david/Documents/";
 	private File config;
 	private String user;
 	private String token;
@@ -25,9 +21,8 @@ public class QuoteBot extends TelegramLongPollingBot {
 	public QuoteBot()
 	{
 		super();
-		workingDir = "/home/david/Documents/GitHub/Telegram-QuoteBot/";
 		try {
-			config = new File(workingDir + "config.txt");
+			config = new File(confDir + "config.txt");
 			Scanner reader = new Scanner(config);
 			user = reader.next();
 			token = reader.next();
@@ -65,13 +60,11 @@ public class QuoteBot extends TelegramLongPollingBot {
 		}
 
 		// Check for if the bot is alive
-		if(message.equals("!status"))
+		if(message.toLowerCase().equals("hi bot"))
 		{
 			SendMessage status = new SendMessage() // Create a SendMessage object
                     .setChatId(update.getMessage().getChatId())
-                    .setText("Ready to start quoting autistic statements.\n"
-						+ "To add a quote for the bot to remember, " 
-						+ "forward the original message containing the quote to it.");
+                    .setText("Ready to start accepting snafus, complaints, and band names.\n");
 			try {
 				sendMessage(status); // Call method to send the message
 				System.out.println("Message sent out at " + LocalDateTime.now());
@@ -79,66 +72,6 @@ public class QuoteBot extends TelegramLongPollingBot {
 				e.printStackTrace();
 			}
 		}
-		/*
-		String[] words = message.split(" ");
-		// TODO: Add recordkeeping for quotes (use forwarded messages)
-		if(words[0].equals("!add"))
-		{
-			String name = words[1];
-			String quote = "";
-			for (int i = 2; i < words.length; i++)
-			{
-				quote = quote.concat(words[i] + " ");
-			}
-			// System.out.println(quote);
-		}*/
-		// TODO: File I/O stuff (try to open file, but if it doesn't exist create it and write to it)	
-		if(null != sayer && null != chat)
-		{
-			addQuote(chatid, message, name);
-		}
-	}
-	
-	/**
-	 * Adds a new quote to the record for the specified group.
-	 * 
-	 * @param	id		The ID of the chat that the message originated from.
-	 * @param	message		The message to be quoted.
-	 * @param	name	The name of the person being quoted.
-	 */
-	private void addQuote(long id, String message, String name)
-	{
-		System.out.println("Adding quote from chat" + id);
-		String filepath = workingDir + "quotes/" + id;
-		File quotefile = new File(filepath);
-		if (!quotefile.exists())
-		{
-			try {
-				quotefile.createNewFile();
-			} catch (IOException e) {
-				System.out.println("Failed to create quote file for this chat.");
-				e.printStackTrace();
-			}
-		}
-		try {
-			Writer quoteWriter = new BufferedWriter(new FileWriter(filepath, true));
-			quoteWriter.append(name + "\t" + message + "\n");
-			quoteWriter.close();
-			System.out.println("Successfully add quote from chat" + id);
-		} catch (IOException e) {
-			System.out.println("Failed to open quote file for writing.");
-			e.printStackTrace();
-		}
-		
-	}
-	
-	/**
-	 * Retrieves a quote for the record from the specified group and sends a message with it.
-	 * 
-	 */
-	private void sendQuote()
-	{
-		
 	}
 
 	@Override
